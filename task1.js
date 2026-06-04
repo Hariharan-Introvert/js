7b8b62eeac30fd5501802abea13b0cc92eeb6a27
vikramchiranjivi <125923649+vikramchiranjivi@users.noreply.github.com>
2026-06-04 20:43:36 +0530

    first commit

---
+++ task1.js
@@ -0,0 +1,59 @@
+let studentName = "Hari";
+let studentAge = 28;
+let studentCourse = "MERN Stack";
+console.log(studentName);
+console.log(studentAge);
+console.log(studentCourse);
+
+
+var company = "Stackly";
+let salary = 20000;
+const country = "India";
+// Updating values
+company = "Google";
+salary = 30000;
+console.log(company);
+console.log(salary);
+console.log(country);
+
+let userName = prompt("What is your name?");
+console.log("Welcome " + userName);
+
+let knowsJS = confirm("Do you know JavaScript?");
+console.log(knowsJS);
+
+let studentNameInput = prompt("Enter Student Name:");
+let studentAgeInput = prompt("Enter Student Age:");
+let studentCityInput = prompt("Enter Student City:");
+console.log("Name : " + studentNameInput);
+console.log("Age : " + studentAgeInput);
+console.log("City : " + studentCityInput);
+
+let course = "JavaScript";
+alert("Welcome to " + course);
+
+
+document.writeln("Hello Student <br>");
+document.writeln("Welcome to JavaScript");
+
+let favFood = "Chicken Biryani";
+let favColor = "Red";
+let favMovie = "John Wick";
+console.log("Food : " + favFood);
+console.log("Color : " + favColor);
+console.log("Movie : " + favMovie);
+
+
+let name = "Hari";
+let age = 28;
+let phone = "9876543210";
+let email = "hari@example.com";
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Phone: " + phone);
+console.log("Email: " + email);
+
+
+let inputName = prompt("Enter Name:");
+let inputCourse = prompt("Enter Course:");
+alert("Hello " + inputName + ", Welcome to " + inputCourse + " Course");
